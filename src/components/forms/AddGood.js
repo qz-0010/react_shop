234f1ddc1fe11d94cac13193669f308aba8e20f7
kzotov <kzotov@hspub.ru>
2018-08-17 18:54:44 +0300

    _

--- src/components/forms/AddGood.js
+++ src/components/forms/AddGood.js
@@ -10,22 +10,20 @@ const AddGood = (props) => {
   };
 
   return (
-    <div>
-      <form action="/admin/good" method="POST" onSubmit={_onSubmit} noValidate>
-        <div>
-          <Input onChange={props.onInputChange} onInit={props.onInputInit} type="text" name="title" placeholder="title" required={true}/>
-        </div>
-        <div>
-          <Input onChange={props.onInputChange} onInit={props.onInputInit} type="number" name="price" placeholder="price" required={true}/>
-        </div>
-        <div>
-          <Input onChange={props.onInputChange} onInit={props.onInputInit} type="file" name="img"/>
-        </div>
-        <div>
-          <input type="submit" />
-        </div>
-      </form>
-    </div>
+    <form action="/admin/good" method="POST" onSubmit={_onSubmit} noValidate>
+      <div>
+        <Input onChange={props.onInputChange} onInit={props.onInputInit} type="text" name="title" placeholder="title" required={true}/>
+      </div>
+      <div>
+        <Input onChange={props.onInputChange} onInit={props.onInputInit} type="number" name="price" placeholder="price" required={true}/>
+      </div>
+      <div>
+        <Input onChange={props.onInputChange} onInit={props.onInputInit} type="file" name="img"/>
+      </div>
+      <div>
+        <input type="submit" />
+      </div>
+    </form>
   );
 };
 
